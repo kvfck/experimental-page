eb05ad2c281a8839c5db83bbacd976e652d05fcb
Karolina <knocek123@o2.pl>
2023-04-02 20:55:51 +0200

    onclick counter button

--- src/App.js
+++ src/App.js
@@ -1,21 +1,15 @@
+import { useState } from 'react';
 import './App.css';
 
-const Person = (props) => {
-  return (
-    <>
-    <h1>Name: {props.name}</h1>
-    <h2>Last name: {props.lastName}</h2>
-    <h2>Age: 30</h2>
-    </>
-    
-  )
-}
 
 const App = () => {
+  const [counter, setCounter] = useState(0);
+
   return (
     <div className="App">
-      <Person name={'John'} lastName={'Wett'}/>
-      <Person name={'Aneta'} lastName={'Wellington'}/>
+      <button onClick={() => setCounter((prevCount) => prevCount - 1)}>-</button>
+      <h1>{counter}</h1>
+      <button onClick={() => setCounter((prevCount) => prevCount + 1)}>+</button>
     </div>
   );
 }
